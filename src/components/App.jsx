e10144d7d581cb34a9c6852bf8d02cf04992b074
Eburu Dearie <eburujacintha@gmail.com>
2023-03-23 23:06:30 +0200

    Fixed a bug

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import { CssBaseline } from '@mui/material';
+// import { Route, Switch} from 'react-router-dom';
 
 const App = () => {
   <div>
-    App
+    <CssBaseline />
+    <h1>Hello App</h1>
   </div>;
 };
 
