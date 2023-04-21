7f72bf3ed06d599841a788dedc5a10467bf31f98
Eburu Dearie <eburujacintha@gmail.com>
2023-04-21 22:22:52 +0200

    Fixed render issue

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import { CssBaseline } from '@mui/material';
 // import { Route, Switch} from 'react-router-dom';
 
-const App = () => {
+const App = () => (
   <div>
     <CssBaseline />
-    <h1>Hello App</h1>
-  </div>;
-};
+    <h1>Hello, World - Filmpire</h1>
+  </div>
+);
 
 export default App;
 
